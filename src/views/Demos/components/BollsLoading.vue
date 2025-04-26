96dbff822206cbbcb36d8fb475843fb210a5692d
changliu35 <changliu35@iflytek.com>
2025-04-26 23:54:40 +0800

    feat: 新增小球loading动画

--- src/views/Demos/components/BollsLoading.vue
+++ src/views/Demos/components/BollsLoading.vue
@@ -1,3 +1,8 @@
+/**
+* @file BollsLoading.vue
+* @description 双色小球旋转动画效果
+* @date 2025-04-26
+*/
 <template>
   <div class="bolls-loading">
     <div v-for="item in 36" :key="item" class="bolls-loading-item"></div>
@@ -5,37 +10,113 @@
 </template>
 
 <script setup lang='ts'>
-
 </script>
 
 <style lang="scss" scoped>
-$bolls-loading-size: 100px; // 圆环大小
-$bolls-loading-item-diameter: 4px; // 小球直径
-$bolls-loading-item-count: 36; // 小球数量
+@use "sass:math"; // 引入sass的math模块
+
+$containerSize: 200px; // 圆环容器大小
+$bollSize: 10px; // 小球直径
+$bollCount: 36; // 小球数量
+$duration: 2s; // 动画持续时间
 
 .bolls-loading {
   position: relative;
-  width: $bolls-loading-size;
-  height: $bolls-loading-size;
+  width: $containerSize;
+  height: $containerSize;
   border-radius: 50%;
-  border: 1px solid red;
+  background: lightblue;
 
   .bolls-loading-item {
-    width: $bolls-loading-item-diameter;
-    height: $bolls-loading-item-diameter;
+    width: $bollSize;
+    height: $bollSize;
     border-radius: 50%;
-    border: 1px solid #000000;
     position: absolute;
-    top: 0;   
+    top: 0;
     left: 50%;
-    margin-left: -$bolls-loading-item-diameter / 2;
-    margin-top: -$bolls-loading-item-diameter / 2;
-    transform-origin: center ($bolls-loading-size / 2 + $bolls-loading-item-diameter / 2); // 设置旋转中心点为圆环中心
+    margin-left: math.div(-$bollSize, 2);
+    margin-top: math.div(-$bollSize, 2);
+    transform-origin: center (math.div($containerSize, 2) + math.div($bollSize, 2));
+    perspective: 100px; // 添加透视效果
+    transform-style: preserve-3d; // 保持3D效果
 
-    /* 循环生成小球 */
-    @for $i from 1 through $bolls-loading-item-count {
+
+    /* 遍历设置每个小球的旋转角度和动画延迟 */
+    @for $i from 1 through $bollCount {
       &:nth-child(#{$i}) {
-        transform: rotate(($i - 1) * (360deg / $bolls-loading-item-count));
+        transform: rotate(($i - 1) * math.div(360deg, $bollCount));
+
+        &::before,
+        &::after {
+          animation-delay: - math.div($duration, $bollCount) * ($i - 1) * 6;
+        }
+      }
+    }
+
+    /* 两种颜色小球的公共样式 */
+    &::before,
+    &::after {
+      position: absolute;
+      content: '';
+      width: 100%;
+      height: 100%;
+      border-radius: 50%;
+    }
+
+    /* 黑色小球 */
+    &::before {
+      top: -100%;
+      background-color: #000000;
+      animation: move-black $duration infinite;
+
+      @keyframes move-black {
+        0% {
+          animation-timing-function: ease-in;
+        }
+
+        25% {
+          transform: translate3d(0, 100%, $bollSize);
+          animation-timing-function: ease-out;
+        }
+
+        50% {
+          transform: translate3d(0, 200%, 0);
+          animation-timing-function: ease-in;
+        }
+
+        75% {
+          transform: translate3d(0, -100%, -$bollSize);
+          animation-timing-function: ease-out;
+        }
+      }
+    }
+
+    /* 绿色小球 */
+    &::after {
+      background-color: greenyellow;
+      top: 100%;
+      animation: move-greenyellow $duration infinite;
+
+
+      @keyframes move-greenyellow {
+        0% {
+          animation-timing-function: ease-in;
+        }
+
+        25% {
+          transform: translate3d(0, -100%, -$bollSize);
+          animation-timing-function: ease-out;
+        }
+
+        50% {
+          transform: translate3d(0, -200%, 0);
+          animation-timing-function: ease-in;
+        }
+
+        75% {
+          transform: translate3d(0, -100%, $bollSize);
+          animation-timing-function: ease-out;
+        }
       }
     }
   }
